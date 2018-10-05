f5af9b2a83604694caff23cd7182edf62f465868
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-05 11:27:24 -0500

    chore: ejemplo metodos estaticos

---
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/metodoestatico/App.java
@@ -0,0 +1,12 @@
+package co.edu.sena.ejemplopoo.metodoestatico;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Calculadora.suma(2, 2);
+	System.out.println(Math.PI);
+
+	}
+
+}
---
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/metodoestatico/Calculadora.java
@@ -0,0 +1,21 @@
+package co.edu.sena.ejemplopoo.metodoestatico;
+
+public class Calculadora {
+
+	public static void suma(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public  static void resta(int a, int b) {
+		System.out.println(a - b);
+	}
+
+	public static void multiplicacion(int a, int b) {
+		System.out.println(a * b);
+	}
+
+	public static void division(int a, int b) {
+		System.out.println(a / b);
+	}
+
+}
