60c7c48ac8a68be23fc606c57f57d8c84e51044f
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-05 11:08:26 -0500

    chore: ejemplo de atributos estaticos

---
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/aributosestaticos/App.java
@@ -0,0 +1,10 @@
+package co.edu.sena.ejemplopoo.aributosestaticos;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(Matematica.PI_CONSTANTE);
+
+	}
+
+}
---
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/aributosestaticos/Matematica.java
@@ -0,0 +1,8 @@
+package co.edu.sena.ejemplopoo.aributosestaticos;
+
+public class Matematica {
+	
+	public final static double PI_CONSTANTE = 3.1415161616161;
+	
+
+}
