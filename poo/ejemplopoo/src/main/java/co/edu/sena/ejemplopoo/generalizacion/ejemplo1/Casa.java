78320e728e0357253d44b27741d3a4ebef8ccbc7
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-05 11:45:06 -0500

    chore: ejemplo clase final

--- poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/generalizacion/ejemplo1/Casa.java
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/generalizacion/ejemplo1/Casa.java
@@ -1,6 +1,6 @@
 package co.edu.sena.ejemplopoo.generalizacion.ejemplo1;
 
-public class Casa extends Vivienda{
+public final class Casa extends Vivienda{
 	
 
 }
---
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/generalizacion/ejemplo1/CasaPrefabricada.java
@@ -0,0 +1,5 @@
+package co.edu.sena.ejemplopoo.generalizacion.ejemplo1;
+
+public class CasaPrefabricada { // si generaliza de casa pues no pues y genera error
+
+}
