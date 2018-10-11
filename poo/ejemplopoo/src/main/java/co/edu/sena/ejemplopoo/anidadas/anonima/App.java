39224978e16795d810f8938d42619d14344ebe53
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-11 14:55:07 -0500

    chore: ejemplos de inner class anonima

---
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/anidadas/anonima/App.java
@@ -0,0 +1,69 @@
+package co.edu.sena.ejemplopoo.anidadas.anonima;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        InterfaceCarro carro = new InterfaceCarro() {// clase contenida anomina
+            public void frenar() {
+                System.out.println("frene");
+            }
+
+            public void prender() {
+                System.out.println("rummm");
+            }
+
+            public void acelerar() {
+                System.out.println("rum rum");
+            }
+        };
+        carro.acelerar();
+        carro.frenar();
+        carro.prender();
+
+        InterfaceCarro carro2 = new InterfaceCarro() {
+            public void frenar() {
+                System.out.println("frenando");
+            }
+
+            public void prender() {
+                System.out.println("encendido");
+            }
+
+            public void acelerar() {
+                System.out.println("aumento la velocidad");
+            }
+        };
+
+
+        System.out.println(carro);
+        System.out.println(carro2);
+
+        List<Carro> lista = new ArrayList<Carro>();
+        lista.add(new Carro("abc-124","mazda"));
+        lista.add(new Carro("abc-127","hyundai"));
+        lista.add(new Carro("abc-124","chevrolet"));
+        lista.add(new Carro("abc-345","mazda"));
+        lista.add(new Carro("abc-667","ford"));
+        lista.add(new Carro("abc-098","audi"));
+        System.out.println(lista.getClass().getCanonicalName());
+        Collections.sort(lista, new Comparator<Carro>() {
+            public int compare(Carro o1, Carro o2) {
+                return o1.getMarca().compareTo(o2.getMarca());
+            }
+        });
+
+        for (Carro c: lista
+        ) {
+            System.out.println(c.getMarca());
+        }
+
+
+
+
+    }
+}
---
+++ poo/ejemplopoo/src/main/java/co/edu/sena/ejemplopoo/anidadas/anonima/InterfaceCarro.java
@@ -0,0 +1,7 @@
+package co.edu.sena.ejemplopoo.anidadas.anonima;
+
+public interface InterfaceCarro {
+    public void frenar();
+    public void prender();
+    public void acelerar();
+}
