cbc676aa74fdf9c792b58566a313825d9aab2f65
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-25 08:05:57 -0500

    chore: ejemplo capitulo08 argumento del metodo main

---
+++ java/fundamentals/src/main/java/co/edu/sena/fundamentals/capitulo08/Ejemplo01.java
@@ -0,0 +1,17 @@
+package co.edu.sena.fundamentals.capitulo08;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        int [] arreglo = new int[10];
+        arreglo[0]= 3;
+        arreglo[1]= 3;
+        arreglo[2]= 3;
+        arreglo[3]= 3;
+        arreglo[4]= 3;
+
+        int [] arreglo2 = {1,2,4,5,6,6,7,7,};
+
+        System.out.println(arreglo2[5]);
+
+    }
+}
---
+++ java/fundamentals/src/main/java/co/edu/sena/fundamentals/capitulo08/Ejemplo02.java
@@ -0,0 +1,10 @@
+package co.edu.sena.fundamentals.capitulo08;
+
+public class Ejemplo02 {
+    public static void main(String[] args) {
+        for (int i = 0; i <args.length ; i++) {
+            System.out.println(args[i]);
+        }
+
+    }
+}
