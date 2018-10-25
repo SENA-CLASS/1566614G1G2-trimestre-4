232e87b576e30d3f875b2902ed19a2122e2eb3f5
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-25 08:09:57 -0500

    chore: ejemplo capitulo08 arreglos en una dimensión

---
+++ java/fundamentals/src/main/java/co/edu/sena/fundamentals/capitulo08/Ejemplo03.java
@@ -0,0 +1,14 @@
+package co.edu.sena.fundamentals.capitulo08;
+
+public class Ejemplo03 {
+    public static void main(String[] args) {
+        String a = "2";
+
+        int aNumero= Integer.parseInt(a);
+        float bNumero = Float.parseFloat(a);
+        String bol="true";
+        boolean boleano = Boolean.parseBoolean(bol);
+        System.out.println(boleano);
+
+    }
+}
