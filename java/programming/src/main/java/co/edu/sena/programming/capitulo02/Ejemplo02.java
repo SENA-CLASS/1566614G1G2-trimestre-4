1bcc53ca7b7a9504dbad20091969d14788a6ad84
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-29 07:54:11 -0500

    chore: literales

---
+++ java/programming/src/main/java/co/edu/sena/programming/capitulo02/Ejemplo02.java
@@ -0,0 +1,22 @@
+package co.edu.sena.programming.capitulo02;
+
+public class Ejemplo02 {
+    public static void main(String[] args) {
+        //literales numericos
+        float a = 23.5F;
+
+        long b = 32_423_467_876L;
+
+        // literales para binarios
+        int binario = 0b1010101;
+        System.out.println(binario);
+
+        // hexadecimales
+        int heaxadecimal = 0xFFFF;
+        System.out.println(heaxadecimal);
+
+        // octal
+        int octal = 071;
+        System.out.println(octal);
+    }
+}
