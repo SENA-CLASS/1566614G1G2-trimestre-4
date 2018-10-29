5c188c807bccd97eb0e778c5cf8c8aa05dcc60a9
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-29 08:47:07 -0500

    chore: capitulo 3

--- java/programming/src/main/java/co/edu/sena/programming/capitulo03/Ejemplo01.java
+++ java/programming/src/main/java/co/edu/sena/programming/capitulo03/Ejemplo01.java
@@ -1,10 +1,20 @@
 package co.edu.sena.programming.capitulo03;
 
 public class Ejemplo01 {
-    public static void main(String[] args) {
-        Perro p = new Perro(1,"firulais");
+    public static void main(String... args) {
+        Perro p = new Perro(1, "firulais");
 
         Perro p2 = new Perro(p.getId(), "rin tin tin");
 
+        System.out.println(calcular(1, 2, 3, 4, 5, 6, 7));
+
+    }
+
+    public static int calcular(int... a) {
+        int suma = 0;
+        for (int i = 0; i < a.length; i++) {
+            suma += a[i];
+        }
+        return suma;
     }
 }
