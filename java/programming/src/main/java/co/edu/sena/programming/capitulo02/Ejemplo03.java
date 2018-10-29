678b6b6642280b7f5c6dc9840b91e161c1a20156
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-29 08:03:46 -0500

    chore: instanceof

---
+++ java/programming/src/main/java/co/edu/sena/programming/capitulo02/Ejemplo03.java
@@ -0,0 +1,8 @@
+package co.edu.sena.programming.capitulo02;
+
+public class Ejemplo03 {
+    public static void main(String[] args) {
+        String a = "hola mundo";
+        System.out.println(a instanceof String ? "es una String": "no es un String");
+    }
+}
