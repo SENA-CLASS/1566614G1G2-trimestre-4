f9534ea154f3d04dbcd2c328653f71cd90fd9d29
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-10-29 08:31:14 -0500

    chore: inmutabilidad

---
+++ java/programming/src/main/java/co/edu/sena/programming/capitulo03/Ejemplo01.java
@@ -0,0 +1,10 @@
+package co.edu.sena.programming.capitulo03;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        Perro p = new Perro(1,"firulais");
+
+        Perro p2 = new Perro(p.getId(), "rin tin tin");
+
+    }
+}
---
+++ java/programming/src/main/java/co/edu/sena/programming/capitulo03/Perro.java
@@ -0,0 +1,19 @@
+package co.edu.sena.programming.capitulo03;
+
+public class Perro {
+    private int id;
+    private String nombre;
+
+    public Perro(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
