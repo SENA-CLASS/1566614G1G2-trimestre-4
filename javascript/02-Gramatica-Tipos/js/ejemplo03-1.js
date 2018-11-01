6189b9ce17254faa096ad8a1e66acb27ea30e52e
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-01 13:16:15 -0500

    chore: ejemplo javascript declaraciones

--- javascript/02-Gramatica-Tipos/js/ejemplo03-1.js
+++ javascript/02-Gramatica-Tipos/js/ejemplo03-1.js
@@ -1,3 +1,4 @@
+let a =15;
 (function () {
 
 })();
--- javascript/02-Gramatica-Tipos/js/ejemplo03-2.js
+++ javascript/02-Gramatica-Tipos/js/ejemplo03-2.js
@@ -1,1 +1,13 @@
 console.log(a);
+var r =12;
+x=50;
+
+function f() {
+    var x=100;
+}
+f();
+
+var y;
+console.log(y);
+console.log(y && true);
+console.log(y +1);
