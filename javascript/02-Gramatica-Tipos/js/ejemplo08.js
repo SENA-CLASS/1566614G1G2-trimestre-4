fc68a1d09313bd6a4713442a6886469f843203be
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-01 14:47:08 -0500

    chore: ejemplo javascript conversion de int o float

---
+++ javascript/02-Gramatica-Tipos/js/ejemplo08.js
@@ -0,0 +1,11 @@
+(function () {
+    /* let numeroLeido= parseInt(prompt("digite un numero"));
+     for (let i = 1; i <=numeroLeido ; i++) {
+         document.write(i+"</br>");
+     }*/
+
+    let numero2 = parseFloat(prompt("digite el real"));
+    document.write(typeof numero2 + "</br>");
+
+
+})();
