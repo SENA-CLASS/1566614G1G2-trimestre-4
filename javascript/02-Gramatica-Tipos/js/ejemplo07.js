f821506c55fdc2706ef3e444af0314359849c19f
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-01 14:17:29 -0500

    chore: ejemplo javascript tipos de datos

---
+++ javascript/02-Gramatica-Tipos/js/ejemplo07.js
@@ -0,0 +1,29 @@
+// tipos de datos
+(function () {
+    //primiivos
+    var a = true;
+    document.write(typeof a + "</br>");
+
+    let b = null;
+    document.write(typeof b + "</br>");
+    console.log(typeof b);
+
+    let c;
+    document.write(typeof c + "</br>");
+
+    let d = 50.7;
+    document.write(typeof d + "</br>");
+
+    let e = "hola";
+    document.write(typeof e + "</br>");
+
+    let f = Symbol("foo");
+    document.write(typeof f + "</br>");
+
+    let g = {
+        marca: "mazda"
+    }
+    document.write(typeof g);
+
+})();
+
