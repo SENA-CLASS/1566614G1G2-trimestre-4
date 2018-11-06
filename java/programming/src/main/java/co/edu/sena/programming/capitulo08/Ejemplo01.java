51f304193f5dab4eedf239358eaba4ba19d9ed89
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-06 07:00:43 -0500

    chore: capitulo 9 argumentos del metodo main

---
+++ java/programming/src/main/java/co/edu/sena/programming/capitulo08/Ejemplo01.java
@@ -0,0 +1,11 @@
+package co.edu.sena.programming.capitulo08;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+
+        if (args[0].equals("--version")){
+            System.out.println("la version es 1.0.0");
+        }
+
+    }
+}
