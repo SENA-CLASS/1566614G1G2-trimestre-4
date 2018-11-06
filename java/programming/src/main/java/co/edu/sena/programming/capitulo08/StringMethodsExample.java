ec76e8b91fd9501f8296d3e4c0bd5213d8e208bb
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-06 07:43:30 -0500

    chore: capitulo 9 metodos con cadenas

---
+++ java/programming/src/main/java/co/edu/sena/programming/capitulo08/StringMethodsExample.java
@@ -0,0 +1,22 @@
+package co.edu.sena.programming.capitulo08;
+
+import java.io.PrintWriter;
+
+public class StringMethodsExample {
+    public static void main(String[] args){
+        PrintWriter pw = new PrintWriter(System.out, true);        
+        String tc01 = "It was the best of times";
+        String tc02 = "It was the worst of times";
+        
+        if (tc01.equals(tc02)){ 
+            pw.println("Strings match..."); }
+            else {
+            pw.println("Strings no match...");
+        }
+        if (tc01.contains("It was")){ 
+            pw.println("It was found"); }
+        String temp = tc02.replace("w", "zw");
+        pw.println(temp);
+        pw.println(tc02.substring(5, 12));
+    }
+}
