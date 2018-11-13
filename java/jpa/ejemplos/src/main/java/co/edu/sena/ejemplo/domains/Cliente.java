c2434ea13bf25ac10b60a8984133d4c8b5db0f82
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-13 08:19:23 -0500

    chore: se crea la entidad cliente

---
+++ java/jpa/ejemplos/src/main/java/co/edu/sena/ejemplo/domains/Cliente.java
@@ -0,0 +1,31 @@
+package co.edu.sena.ejemplo.domains;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id
+    private int id;
+
+    private String nombre;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
