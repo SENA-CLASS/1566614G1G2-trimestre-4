31de1d2c5c36049e784b8be45e26b0c9ac5de858
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-21 10:54:46 -0500

    chore: clase principal de spring boot

---
+++ java/jpa/sprintboot/senaceet2/src/main/java/co/edu/sena/senaceet2/Senaceet2Application.java
@@ -0,0 +1,12 @@
+package co.edu.sena.senaceet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Senaceet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Senaceet2Application.class, args);
+	}
+}
