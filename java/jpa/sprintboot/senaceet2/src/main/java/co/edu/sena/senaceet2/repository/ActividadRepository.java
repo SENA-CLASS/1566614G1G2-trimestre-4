4f13916f02267f78d71a809ed10c7dc0ea22953b
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-21 11:15:33 -0500

    chore: repositorio actividad

---
+++ java/jpa/sprintboot/senaceet2/src/main/java/co/edu/sena/senaceet2/repository/ActividadRepository.java
@@ -0,0 +1,7 @@
+package co.edu.sena.senaceet2.repository;
+
+import co.edu.sena.senaceet2.domain.Actividad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActividadRepository extends JpaRepository<Actividad, Integer> {
+}
