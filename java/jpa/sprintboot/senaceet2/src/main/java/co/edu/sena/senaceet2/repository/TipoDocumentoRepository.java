2e8f872c6f89347a034cbdbaaffe2d8bb450d0f5
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-21 11:15:53 -0500

    chore: repositorio tipo de documento

---
+++ java/jpa/sprintboot/senaceet2/src/main/java/co/edu/sena/senaceet2/repository/TipoDocumentoRepository.java
@@ -0,0 +1,8 @@
+package co.edu.sena.senaceet2.repository;
+
+import co.edu.sena.senaceet2.domain.TipoDocumento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoDocumentoRepository extends JpaRepository<TipoDocumento, Integer> {
+
+}
