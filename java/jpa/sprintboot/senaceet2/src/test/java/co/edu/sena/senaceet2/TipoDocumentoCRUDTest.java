abbe9449a61e5aa9cf9d2ef977ac46ae16cdd796
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-22 08:09:04 -0500

    chore: ejemplo ruebas de los querys methods

--- java/jpa/sprintboot/senaceet2/src/test/java/co/edu/sena/senaceet2/TipoDocumentoCRUDTest.java
+++ java/jpa/sprintboot/senaceet2/src/test/java/co/edu/sena/senaceet2/TipoDocumentoCRUDTest.java
@@ -3,22 +3,23 @@ package co.edu.sena.senaceet2;
 import co.edu.sena.senaceet2.domain.TipoDocumento;
 import co.edu.sena.senaceet2.repository.TipoDocumentoRepository;
 import co.edu.sena.senaceet2.util.DateUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class TipoDocumentoCRUDTest {
 
-    @Autowired
-    TipoDocumentoRepository tipoDocumentoRepository;
+    @Autowired// inyecccion de dependias
+    private  TipoDocumentoRepository tipoDocumentoRepository;
 
     @Test
     public void test01Insert() {
@@ -26,12 +27,29 @@ public class TipoDocumentoCRUDTest {
         tipoDocumento.setSigla("CC");
         tipoDocumento.setNombreDocumento("Cedula de ciudadania");
         tipoDocumento.setEstado("Activa");
-
         tipoDocumentoRepository.save(tipoDocumento);
-
-        Date fechavieja = DateUtils.asDate(LocalDate.of(2108,12,11));
-
-
+        Assert.assertEquals(tipoDocumento, tipoDocumentoRepository.findBySigla("CC"));
     }
 
+    @Test
+    public void test02UpdateNombreDocumento() {
+        TipoDocumento tipoDocumento = tipoDocumentoRepository.findBySigla("CC");
+        tipoDocumento.setNombreDocumento("Cedula Colombiana");
+        tipoDocumentoRepository.save(tipoDocumento);
+        Assert.assertEquals(tipoDocumentoRepository.findBySigla("CC").getNombreDocumento(),"Cedula Colombiana");
+    }
+
+    @Test
+    public void test03UpdateSigla() {
+        TipoDocumento tipoDocumento = tipoDocumentoRepository.findBySigla("CC");
+        tipoDocumento.setSigla("CCC");
+        tipoDocumentoRepository.save(tipoDocumento);
+        Assert.assertEquals(tipoDocumentoRepository.findById(tipoDocumento.getId()).get().getSigla(),"CCC");
+    }
+
+    @Test
+    public void test04FindAll() {
+        List<TipoDocumento> list = tipoDocumentoRepository.findAll();
+        Assert.assertFalse(list.isEmpty());
+    }
 }
