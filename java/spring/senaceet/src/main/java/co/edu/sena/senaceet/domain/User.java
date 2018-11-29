f2b15d8177613660274c7b3c7decc319c165bfb2
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-11-29 08:37:28 -0500

    chore: se cambia el nombre de las tablas

--- java/spring/senaceet/src/main/java/co/edu/sena/senaceet/domain/User.java
+++ java/spring/senaceet/src/main/java/co/edu/sena/senaceet/domain/User.java
@@ -24,7 +24,7 @@ import java.time.Instant;
  * A user.
  */
 @Entity
-@Table(name = "jhi_user")
+@Table(name = "usuario")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class User extends AbstractAuditingEntity implements Serializable {
 
@@ -79,7 +79,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @JsonIgnore
     @ManyToMany
     @JoinTable(
-        name = "jhi_user_authority",
+        name = "usuario_has_roles",
         joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
         inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")})
     @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
